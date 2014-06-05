5ecc3831a2aee84812a53b35a004219ac650338c
Nikos Aréchiga <narechig@andrew.cmu.edu>
2014-06-04 23:02:45 -0700

    Finalized simple parser prototype. Finally.

--- src/EITool.java
+++ src/EITool.java
@@ -8,6 +8,14 @@ class EITool {
 	public static void main ( String [] args ) {
 
 		System.out.println("Hello world!");
+		String filename = args[0];
+
+		System.out.println("My argument is: " + filename);
+
+		System.out.println("The control envelope is: " + Parser.parseEnvelope( filename ) );
+		System.out.println("The invariant is: " + Parser.parseInvariant( filename ));
+		System.out.println("The robust parameters are: " + Parser.parseRobustParameters( filename ));
+		System.out.println("The control law is: " + Parser.parseControlLaw( filename ));
 
 
 	}
--- src/Parser.java
+++ src/Parser.java
@@ -13,85 +13,74 @@ public static String parseItem( String inputFile, String item ) {
 
 	// The patterns we will check for
 	Pattern labelPattern = Pattern.compile( item );
-	Pattern openBracePattern = Pattern.compile("{");
-	Pattern textPattern = Pattern.compile("[^}]*");
-	Pattern closeBracePattern = Pattern.compile("}");
+	Pattern textPattern = Pattern.compile("[^\\}\\s]*");
+	Pattern endPattern = Pattern.compile("end");
 
 	// States of the parser state machine
-	int INIT	=	0;
-	int LABELFOUND	=	1;
-	int OPENBRACE	=	2;
-	int TEXT	=	3;
-	int CLOSEBRACE	=	4;
-	int END		=	CLOSEBRACE;
-	int ERROR	=	-1;
+	int INIT		=	1;
+	int LABELFOUND		=	2;
+	int TEXTFOUND		=	3;
+	int END			=	4;
 	
 	int STATE = INIT;
 
-	// assorted administrative overhead variables
-	int labelEnd = 1000000;
-	int openBraceEnd = 1000000;
-	int textEnd = 1000000;
-	int closeBraceEnd = 1000000;
-
 	try {
+		//System.out.println("Opening file scanner...");
 		Scanner scan = new Scanner( new BufferedReader( new FileReader( inputFile ) ) );
 	
-		while ( (scan.hasNext()) && (STATE!= END) && (STATE != ERROR) ) {
+		while ( (scan.hasNext()) && (STATE!= END) ) {
 			String tmpString = scan.next();
+			tmpString = tmpString.replace("\\n", ""); // Kill the evil newlines
+
+			//System.out.println("Input line is: " + tmpString + "; Current state is: " + STATE);
 
 			// Run the matchers
 			Matcher labelMatcher = labelPattern.matcher( tmpString );
-			Matcher openBraceMatcher = openBracePattern.matcher( tmpString );
 			Matcher textMatcher = textPattern.matcher( tmpString );
-			Matcher closeBraceMatcher = closeBracePattern.matcher( tmpString );
+			Matcher endMatcher = endPattern.matcher( tmpString );
+			
+			switch( STATE ) {
 
-			if ( STATE == INIT ) {
-				if ( labelMatcher.find() ) {
-					labelEnd = labelMatcher.end();
-					STATE = LABELFOUND;
-				}
-			}
+				case 1: // INIT
+					//System.out.println("Parser automaton initialized");
+					if ( labelMatcher.find() ) {
+						STATE = LABELFOUND;
 
-			if ( STATE == LABELFOUND ) {
-				while ( openBraceMatcher.find() ) { //Cycle through open braces
-					if ( openBraceMatcher.start() > labelEnd ) {
-						openBraceEnd = openBraceMatcher.end();
-						STATE = OPENBRACE;
+					//	System.out.println("Label found, ending at position " + labelMatcher.end() + ": " + labelMatcher.group());
 					}
-				}
+					break;
 
-			}
+				case 2: // LABELFOUND
+					if ( textMatcher.find() ) {
+						STATE = TEXTFOUND;
 
-			if ( STATE == OPENBRACE ) {
-				while ( textMatcher.find() ) {
-					if ( textMatcher.start() > openBraceEnd ) {
-						text = text + textMatcher.group();
-						textEnd = textMatcher.end();
-						STATE = TEXT;
+						text = textMatcher.group();
+					//	System.out.println("Text found, ending at position " + textMatcher.end() + ": " + text );
 					}
+					break;
+					
+
+				case 3: // TEXTFOUND
+
+					if ( endMatcher.find() ) {
+						STATE = END;
+
+					//	System.out.println("End found, ending at position " + endMatcher.end() + ": " + endMatcher.group() );
+					} else if ( textMatcher.find() ) {
+						text = text + " " + textMatcher.group();
+						STATE = TEXTFOUND;
+
+					//	System.out.println("Text found, ending at position " + textMatcher.end());
+					//	System.out.println("Text is: " + text );
+					} 
+
+					break;
+
+				case 4: // END
+					break;
+
 				}
-			}
-
-			if ( STATE == TEXT ) {
-				while ( closeBraceMatcher.find() ) {
-					if ( closeBraceMatcher.start() > textEnd ) {
-						closeBraceEnd = closeBraceMatcher.end();
-						STATE = CLOSEBRACE;
-					}
-				}
-
-			}
-			if ( STATE == CLOSEBRACE ) {
-				//Do nothing, we are done here
-			}
-			if ( STATE == ERROR ) {
-				System.out.println("Error!");
-				return null;
-			}
-
 		}
-
 	} catch ( Exception e ) {
 		System.out.println( e );
 	}
@@ -101,24 +90,28 @@ public static String parseItem( String inputFile, String item ) {
 
 public static String parseEnvelope( String inputFile ) {
 	
-	return parseItem( inputFile, "\\\\envelope" );
+	//System.out.println("Starting parse for envelope...");
+	return parseItem( inputFile, "envelope:" );
 }
 
 public static String parseInvariant( String inputFile ) {
 
-	return parseItem( inputFile, "\\\\invariant" );
+	//System.out.println("Starting parse for invariant...");
+	return parseItem( inputFile, "invariant:" );
 
 }
 
 public static String parseRobustParameters( String inputFile ) {
 
-	return parseItem( inputFile, "\\\\robustparameters" );
+	//System.out.println("Starting parse for robust parameters...");
+	return parseItem( inputFile, "robustparameters:" );
 
 }
 
 public static String parseControlLaw( String inputFile ) {
 
-	return parseItem( inputFile, "\\\\robustparameters" );
+	//System.out.println("Starting parse for control law...");
+	return parseItem( inputFile, "controllaw:" );
 
 }
 
